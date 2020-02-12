04a5df1ca01d6ed22ace9d693c67a5e0096937d3
Apoorv Sohal <apoorvsohal@protonmail.com>
2020-02-12 17:26:56 +0530

    finished book title test

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
 # write your code here
+  attr_accessor :title
+
+  def title 
+    articles_to_avoid = ['and','the','a','an','in','of']
+    words = @title.split(' ')
+    words = [words[0].capitalize] + words[1..-1].map{|x| (articles_to_avoid.include?(x) ? x : x.capitalize)}
+    @title = words.join(' ')
+  end
+
 end
